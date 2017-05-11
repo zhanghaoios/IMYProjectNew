7fa77cf21bed79343187144983426fa7a560f11c
张浩 <zhanghao@zhanghaodeMacBook-Pro.local>
2017-05-11 16:33:35 +0800

    Initial Commit

---
+++ IMYProjectNew/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IMYProjectNew
+//
+//  Created by 张浩 on 2017/5/11.
+//  Copyright © 2017年 张浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IMYProjectNew/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IMYProjectNew
+//
+//  Created by 张浩 on 2017/5/11.
+//  Copyright © 2017年 张浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
